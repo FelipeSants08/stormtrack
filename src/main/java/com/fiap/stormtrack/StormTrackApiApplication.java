7b5316a98999bd0ed1ee7a57fa726128a2e91bde
FelipeSants08 <felipe_santana.01@hotmail.com>
2025-05-30 12:27:47 -0300

    Crição de Cors, e rodando projeto

---
+++ src/main/java/com/fiap/stormtrack/StormTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.stormtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormTrackApiApplication.class, args);
+	}
+
+}
